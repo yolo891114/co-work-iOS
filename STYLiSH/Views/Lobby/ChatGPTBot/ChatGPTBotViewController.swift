f95486631d1b026e52f72476f7f389073f81e0b0
jeff <omailong@ssy.local>
2023-09-05 16:38:39 +0800

    FEAT: Established ChatGPT image analysis

--- STYLiSH/Views/Lobby/ChatGPTBot/ChatGPTBotViewController.swift
+++ STYLiSH/Views/Lobby/ChatGPTBot/ChatGPTBotViewController.swift
@@ -16,6 +16,18 @@ class ChatGPTBotViewController: UIViewController {
     @IBOutlet weak var chatTextField: UITextField!
     
     @IBOutlet weak var sendButton: UIButton!
+
+    @IBOutlet weak var cameraButton: UIButton!
+    
+    @IBAction func cameraButtonTapped(_ sender: UIButton) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+        
+    }
+    var selectPhoto = false // 判定是否有選擇照片
     
     var messages: [String] = []
     
@@ -30,7 +42,7 @@ class ChatGPTBotViewController: UIViewController {
         
         sendButton.addTarget(self, action: #selector(sendButtonTapped), for: .touchUpInside)
         
-        chatTableView.estimatedRowHeight = 50
+        chatTableView.estimatedRowHeight = 60
         chatTableView.rowHeight = UITableView.automaticDimension
     }
     
@@ -83,6 +95,7 @@ extension ChatGPTBotViewController {
     
     func postChatApi(indexPath: IndexPath) {
         LKProgressHUD.show()
+        sendButton.isEnabled = false
         if let url = URL(string: "http://54.66.20.75:8080/api/1.0/recommendation/chatbox") {
             var request = URLRequest(url: url)
             
@@ -94,6 +107,7 @@ extension ChatGPTBotViewController {
             
             URLSession.shared.dataTask(with: request) { data, response, error in
                 LKProgressHUD.dismiss()
+                
                 if let data = data {
                     let content = String(data: data, encoding: .utf8)
                     print("Raw Data Received:")
@@ -104,6 +118,7 @@ extension ChatGPTBotViewController {
                         
                         
                         DispatchQueue.main.async {
+                            self.sendButton.isEnabled = true
                             self.messages.append(chatResponse.chatResponse)
                             let newIndexPath = IndexPath(row: self.messages.count - 1, section: 0)
                             self.chatTableView.insertRows(at: [newIndexPath], with: .automatic) // 插入新的 cell
@@ -126,4 +141,75 @@ extension ChatGPTBotViewController {
             }.resume()
         }
     }
+}
+
+
+extension ChatGPTBotViewController: UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        selectPhoto = true
+        if let image = info[.originalImage] as? UIImage {
+            postImageApi(image: image)
+        }
+        
+        
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+    func postImageApi(image: UIImage) {
+        LKProgressHUD.show()
+        let url = URL(string: "http://54.66.20.75:8080/api/1.0/recommendation/smart_image")
+        var request = URLRequest(url: url!)
+        request.httpMethod = "POST"
+
+        let boundary = "Boundary-\(UUID().uuidString)"
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+
+        var body = Data()
+        
+        if let imageData = image.jpegData(compressionQuality: 1) {
+            body.append("--\(boundary)\r\n")
+            body.append("Content-Disposition: form-data; name=\"image\"; filename=\"image.jpg\"\r\n")
+            body.append("Content-Type: image/jpeg\r\n\r\n")
+            body.append(imageData)
+            body.append("\r\n")
+        }
+        
+        body.append("--\(boundary)--\r\n")
+
+        request.httpBody = body
+
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            LKProgressHUD.dismiss()
+            if let data = data {
+                let content = String(data: data, encoding: .utf8)
+                print("圖片分析結果:")
+                print(content ?? "No data")
+                do {
+                    let decoder = JSONDecoder()
+                    let chatResponse = try decoder.decode(ChatResponse.self, from: data)
+                    
+                    print("Chat Response: \(chatResponse.chatResponse)")
+                    print("Product ID: \(chatResponse.productId)")
+                    
+                } catch let decodeError {
+                    print("Decoding error: \(decodeError)")
+                }
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                print("HTTP Status Code: \(httpResponse.statusCode)")
+            }
+            if let error = error {
+                print("Error when post image API:\(error)")
+                
+            }
+        }.resume()
+    }
+
+        
 }
